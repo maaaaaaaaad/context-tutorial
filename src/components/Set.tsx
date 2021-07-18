b382f606971909ded6c6a3937ddeb0109ac8a64b
DONG WOONG OH <integral5137@gmail.com>
2021-07-19 02:35:15 +0900

    Add to Set.tsx

--- src/components/Set.tsx
+++ src/components/Set.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TextContext } from "../contexts/Text.context";
 
 const Set = () => {
-  return <section></section>;
+  const { state, actions } = useContext(TextContext);
+
+  const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+    //
+    const valueType = e.currentTarget.name;
+    const newValue = e.currentTarget.value;
+
+    if (valueType === "name") {
+      actions.setName(newValue);
+    }
+    //
+    else if (valueType === "age") {
+      actions.setAge(newValue);
+    }
+  };
+
+  return (
+    <form>
+      <input
+        autoComplete="off"
+        minLength={2}
+        name="name"
+        onChange={handleOnChange}
+        placeholder={state.name}
+        type="text"
+        required
+      />
+      <input
+        autoComplete="off"
+        minLength={1}
+        maxLength={2}
+        name="age"
+        onChange={handleOnChange}
+        placeholder={state.age}
+        type="text"
+        required
+      />
+      <input type="submit" value="Save Chagned" />
+    </form>
+  );
 };
 
 export default Set;
